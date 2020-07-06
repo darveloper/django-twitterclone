d33e96975e9d8a60a16bcab82bcbec75b6bab737
darveloper <c.darlyze@gmail.com>
2020-07-06 19:04:25 -0400

    changes

--- twitteruser/views.py
+++ twitteruser/views.py
@@ -6,11 +6,13 @@ from notification.models import Notification
 
 
 @login_required(login_url='/login')
-def index(request):
+def index(request, user_id):
+    user = CustomUser.objects.get(id=user_id)
     following = request.user.following.all()
     tweets = Tweet.objects.filter(author__in=following).order_by("-post_date")
+    user_tweets = Tweet.objects.filter(authot=user).order_by("-post_date")
     notifications = Notification.objects.filter(receiver=request.user)
-    return render(request, 'index.html', {'tweets': tweets, 'notifications': notifications})
+    return render(request, 'index.html', {'tweets': tweets, 'user_tweets':user_tweets, 'notifications': notifications})
 
 def user_view(request, user_id):
     user = CustomUser.objects.get(id=user_id)
